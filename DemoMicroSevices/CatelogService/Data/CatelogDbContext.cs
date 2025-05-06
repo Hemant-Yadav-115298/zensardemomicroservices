9c898238afbe941a86951b2b3ed39c33ffee617c
Hemant-Shashikant-Yadav <hemant.yadav@zensar.com>
2025-05-06 11:49:54 +0530

    stage

---
+++ DemoMicroSevices/CatelogService/Data/CatelogDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CatelogService.Data;
+using CatelogService.Models;
+
+
+namespace CatelogService.Data
+{
+    public class CatelogDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public CatelogDbContext(DbContextOptions<CatelogDbContext>options):base(options) 
+        {
+            
+        }
+    }
+}
