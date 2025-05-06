7135e92a8e18e6f1eae11e1ca7d12c842457b821
Hemant-Shashikant-Yadav <hemant.yadav@zensar.com>
2025-05-06 15:13:04 +0530

    docker added

--- DemoMicroSevices/OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ DemoMicroSevices/OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb47713e434ec69ffdd5faf3ae2b2641bc3c58a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7b81bcdaf7dbd0e8fffebf9e19e201ef854e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
